577397b9d623b9b8ca42b6029fb8e01851bd71bb
Saurav Dhillon <souravdhillon010@gmail.com>
2024-12-12 22:52:49 -0500

    Added 01_seeds.sql which contains INSERT statments for the database

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,25 +1,25 @@
-INSERT INTO users (id, name, email, password)
+INSERT INTO users (name, email, password)
 VALUES 
-(1, 'Eva Stanley', 'evastan21@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
-(2, 'Louisa Meyer', 'louisamey57@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
-(3, 'Dominic Parks','dominic678@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+('Eva Stanley', 'evastan21@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Louisa Meyer', 'louisamey57@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Dominic Parks','dominic678@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
 
-INSERT INTO properties (id, owner_id, title, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
 VALUES 
-(1, 1, 'Spped lamp', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 93061, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', '28142', TRUE),
-(2, 1, 'Blank corner', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234, 6, 6, 7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', '83680', TRUE),
-(3, 2, 'Habit mix', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', '44583', TRUE);
+(1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 93061, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', '28142', TRUE),
+(1, 'Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234, 6, 6, 7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', '83680', TRUE),
+(2, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', '44583', TRUE);
 
-INSERT INTO reservations (id, start_date, end_date, property_id, guest_id)
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
 VALUES 
-(1, '2018-09-11', '2018-09-26', 1, 1),
-(2, '2019-01-04', '2019-02-01', 2, 2),
-(3, '2021-10-01', '2021-10-14', 3, 3);
+('2018-09-11', '2018-09-26', 1, 1),
+('2019-01-04', '2019-02-01', 2, 2),
+('2021-10-01', '2021-10-14', 3, 3);
 
-INSERT INTO property_reviews (id, guest_id, property_id, reservation_id, rating, message)
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
 VALUES 
-(1, 3, 2, 1, 3, 'messages'), 
-(2, 2, 2, 2, 4, 'messages'),
-(3, 3, 1, 3, 4, 'messages');
+(3, 2, 1, 3, 'messages'), 
+(2, 2, 2, 4, 'messages'),
+(3, 1, 3, 4, 'messages');
 
 
